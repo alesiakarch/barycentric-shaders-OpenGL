10322149b55cdf892781c983882579cec43079ce
alesiakarch <karchevskaya.alesya@gmail.com>
2025-01-27 03:58:51 +0000

    went back to 1 shader structure

--- ProjectSetupNGL/shaders/FinalQuadFragmentShader.glsl
+++ ProjectSetupNGL/shaders/FinalQuadFragmentShader.glsl
@@ -5,43 +5,12 @@ layout(location = 0) out vec4 finalColour;
 in vec2 uv;
 
 uniform sampler2D UVTexture;
-// t_i parameters, end goal maps that i expect to combine
-uniform sampler2D DiffuseWeight;
-uniform sampler2D ShadowWeight;
-uniform sampler2D SpecularWeight;
-uniform sampler2D OutlineWeight;
 
-
+// potentially add outline detection
 // control images to be assigned to each weight
-uniform sampler2D diffuseTexture;
-uniform sampler2D shadowTexture;
-uniform sampler2D specularTexture;
-uniform sampler2D outlineTexture;
+uniform sampler2D blendTexture;
 
 void main()
 {
-    //finalColour = texture(UVTexture, uv);
-    // trying to blend diffuse with shadow
-    vec2 meshUV = clamp(texture(UVTexture, uv), 0.0, 1.0).rg; // get the uv coordinates
-
-    float wx = texture(DiffuseWeight, uv).r; // weight for the first texture
-    float wy = texture(ShadowWeight, uv).g; // weight for the second texture
-    float wz = texture(SpecularWeight, uv).b; // weight for the third texture // my barycentric coords basically
-    float ww = texture(OutlineWeight, uv).a; // weight for the fourth texture
-
-    vec3 tex1 = texture(diffuseTexture, meshUV).rgb;
-    vec3 tex2 = texture(shadowTexture, meshUV).rgb;
-    vec3 tex3 = texture(specularTexture, meshUV).rgb;
-    vec3 tex4 = texture(outlineTexture, meshUV).rgb;
-     
-    float totalWeight = wx + wy + wz + ww; // normalizing the weights to 1
-    wx /= totalWeight;
-    wy /= totalWeight;
-    wz /= totalWeight;
-    ww /= totalWeight;
-
-    vec3 blendtex = tex1 * wx + tex2 * wy + tex3 * wz + tex4 * ww; // blending the textures
-    finalColour = vec4(blendtex, 1.0);
-
-    //finalColour = vec4(tex1, 1.0);
+    finalColour = vec4(texture(blendTexture, uv).rgb, 1.0);
 }
--- ProjectSetupNGL/shaders/MeshVertexShader.glsl
+++ ProjectSetupNGL/shaders/MeshVertexShader.glsl
@@ -2,6 +2,8 @@
 
 uniform mat4 MVP;
 uniform mat4 modelMat;
+uniform vec3 lightPos;
+uniform vec3 viewPos;
 
 layout(location = 0) in vec3 vertPos;
 layout(location = 1) in vec3 vertNormal;
@@ -10,6 +12,7 @@ layout(location = 2) in vec2 vertUV;
 out vec3 normal;
 out vec2 uv;
 out vec3 fragPos;
+out vec3 weight;
 
 void main()
 {
@@ -18,6 +21,19 @@ void main()
     fragPos = vec3(modelMat * vec4(vertPos, 1.0));
     normal = normalize(mat3(transpose(inverse(modelMat))) * vertNormal); // tranfsormed from object space to world space
 
-    // weight = vec4(lightIntensity, 1 - lightIntensity, fresnel, spec); // how much light is on the fragment, assuming 2 textures
-    //                                                              // and satifying partition of unity
+    // calcalculate diffuse light
+    vec3 lightDir = normalize(lightPos - fragPos);  
+    float lightIntensity = max(dot(lightDir, normal), 0.0); // calculate how the normals align with the light
+
+    // calculate specular light
+    float specularStrength = 1.0;
+    float shininess = 8.0;
+
+    vec3 viewDir = normalize(viewPos - fragPos);
+    vec3 reflectDir = reflect(-lightDir, normal);  
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), shininess);
+    //vec3 specular = specularStrength * spec * vec3(1.0, 1.0, 1.0); 
+
+    weight = vec3(lightIntensity, 1 - lightIntensity, spec); // how much light is on the fragment, assuming 2 textures
+                                                             // and satifying partition of unity
 }
